6c70910d27d6fbba993279ad0a8611260c80f6f3
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-03-16 20:06:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const mongoURI = 'mongodb+srv://chiraitori:<password>@cluster0.hffrvtj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://chiraitori:chiraitori@cluster0.hffrvtj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true })
     .then(() => console.log("MongoDB Connected"))
